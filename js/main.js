c14d3d2c78b61611d32e2d031017afbd127ca306
Giuseppe <giuseppe.dagostino.d@gmail.com>
2020-12-21 10:30:42 +0100

    Ricerca istantanea

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-// MILESTONE 1
-// Cliccando il  bottone, si devono cercare sull’API tutti i film che contengono ciò che ha scritto l’utente. Sostituiamo quindi le nostre schede con quelle generate dalla chiamata, tralasciando per ora le stelline.
+// MILESTONE 2
+// STELLINE - Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5, lasciando le restanti vuote.
+// BANDIERINE - Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API.
+// SERIE TV - Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
 
 var app = new Vue ({
   el: "#root",
